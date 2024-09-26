0d0de5c6412e3bd2639d43ae5d55693e67efd75d
Morfusee <markvalenzuela.coding@gmail.com>
2024-09-26 22:57:07 +0800

    FEAT: Added Dashboard Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Dashboard from "./pages/dashboard";
 
 function App() {
   const router = createBrowserRouter([
@@ -8,13 +9,19 @@ function App() {
       path: "",
       element: <Login />,
     },
+    {
+      path: "/",
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: "dashboard",
+          element: <Dashboard />,
+        },
+      ],
+    },
   ]);
 
-  return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
