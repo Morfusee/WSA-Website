1cda6be2db3e1e1149a7f99324afb45000940373
Morfusee <markvalenzuela.coding@gmail.com>
2024-10-08 22:48:49 +0800

    FEAT: Provided onClick Attribute for TypeButton

--- src/components/TypeButton.tsx
+++ src/components/TypeButton.tsx
@@ -1,9 +1,11 @@
 function TypeButton({
   label,
   className,
+  onClick,
 }: {
   label: string;
   className?: string;
+  onClick?: () => void;
 }) {
   return (
     <button
@@ -11,6 +13,7 @@ function TypeButton({
         "bg-primary-dark hover:bg-primary-main text-gray-100 rounded-md min-h-0 px-6 py-1.5 " +
         className
       }
+      onClick={onClick}
     >
       {label}
     </button>
