aa1fe1192cf05d66b494b839cb277eb042fcef2a
Morfusee <markvalenzuela.coding@gmail.com>
2024-10-11 21:30:08 +0800

    FEAT (Dashboard): Added Mock Data for Dashboard and Nav

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,6 +2,11 @@ import { Container } from "@mui/material";
 import welcome_logo from "../../assets/images/welcome.svg";
 import logo from "../../assets/images/logo.png";
 import { useBoundStore } from "../../utils/store";
+import { ClothingCategory, IWardrobe } from "../../interfaces/IWardrobe";
+import TopImage from "../../assets/images/top.png";
+import BottomImage from "../../assets/images/bottoms.png";
+import UndergarmentImage from "../../assets/images/undergarments.png";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
   const WardrobeItems = useBoundStore((state) => state.wardrobeItems);
@@ -47,7 +52,7 @@ function WelcomeCard() {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
             doloremque?
           </p>
-          <GoButton />
+          <GoButton id={1} />
         </span>
       </div>
       <div className="flex-[1_1_30%] hidden lg:block">
@@ -67,47 +72,90 @@ function StatisticsCard({ title, value }: { title: string; value: number }) {
 }
 
 function CurrentItemsTable() {
+  const WardrobeItems = useBoundStore((state) => state.wardrobeItems);
+  const currentItems = WardrobeItems.filter(
+    (item) => item.status === "In Laundry"
+  );
+
   return (
     <section className="flex flex-col gap-3.5 p-6 text-white rounded-md shadow-sm bg-primary-dark">
       <h1 className="font-semibold tracking-wide">
         Current Items in the Laundry
       </h1>
-      <CurrentItemsCard />
-      <CurrentItemsCard />
-      <CurrentItemsCard />
+      {currentItems.map((item, index) => (
+        <CurrentItemsCard
+          id={item.id}
+          clothing_category={item.clothing_category}
+          description={item.description}
+          name={item.name}
+          status={item.status}
+          key={item.id}
+        />
+      ))}
     </section>
   );
 }
 
-function CurrentItemsCard() {
+function CurrentItemsCard({
+  id,
+  name,
+  clothing_category,
+  status,
+  description,
+}: Omit<IWardrobe, "last_washed" | "date_added" | "previous_session">) {
+  const handleImagePlaceholder = (category: ClothingCategory) => {
+    switch (category) {
+      case "Top":
+        return TopImage;
+      case "Bottom":
+        return BottomImage;
+      case "Undergarments":
+        return UndergarmentImage;
+      default:
+        return logo;
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-2">
-      <span className="flex px-4 py-2 bg-gray-600 rounded-md items-center gap-4">
-        <img
-          src={logo}
-          alt=""
-          className="w-14 h-10 min-w-14 min-h-10 object-contain bg-gray-700 rounded-md"
-        />
-        <span className="flex flex-col">
-          <h1 className="font-semibold line-clamp-1">
-            Lorem ipsum dolor sit amet.
-          </h1>
-          <p className="text-sm line-clamp-1">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
-            illo.
-          </p>
+    <div
+      // onClick={() => handleCardClick(id)}
+      className="flex px-4 py-2 bg-gray-600 rounded-md items-center gap-4"
+    >
+      <img
+        src={handleImagePlaceholder(clothing_category)}
+        alt="image_placeholder"
+        className="w-14 h-14 min-w-14 min-h-14 p-2 object-contain bg-gray-800 rounded-md"
+      />
+      <span className="flex flex-col">
+        <h1 className="font-semibold line-clamp-1">{name}</h1>
+        <span className="text-sm text-white line-clamp-1 flex">
+          <div className="flex flex-col">
+            <p>{status}</p>
+            <span className="flex items-center gap-1.5">
+              <p>{clothing_category}</p>
+              <span className="size-1 min-w-1 min-h-1 bg-gray-300 rounded-full" />
+              <p className="line-clamp-1">{description}</p>
+            </span>
+          </div>
         </span>
-        <GoButton className="ml-auto" />
       </span>
+      <GoButton className="ml-auto" id={id} />
     </div>
   );
 }
 
-function GoButton({ className }: { className?: string }) {
+function GoButton({ className, id }: { className?: string; id: number }) {
+  const navigate = useNavigate();
+
+  const handleGoButtonClick = () => {
+    navigate(`/wardrobe/${id}`);
+  };
+
   return (
     <button
+      onClick={handleGoButtonClick}
       className={
-        "rounded-md px-4 py-2 w-fit font-semibold text-sm text-white bg-primary-main hover:bg-blue-300 " +
+        "rounded-md px-4 py-2 w-fit font-semibold text-sm text-white bg-primary-main hover:bg-blue-500 " +
         className
       }
     >
